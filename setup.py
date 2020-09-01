bd805641167950860ee8b0ef172b21ab4c3adaf9
Bngzifei <38546885+Bngzifei@users.noreply.github.com>
2020-09-01 19:47:02 +0800

    [FIX]打包成wheel的形式

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='file-diff',
-    version="1.0.3",
+    version="1.0.4",
     description=(
         'Specifies the tool for text content difference detection in the directory'
     ),
